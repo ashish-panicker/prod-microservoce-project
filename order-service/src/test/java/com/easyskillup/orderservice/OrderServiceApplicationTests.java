b12be3b6e240b65b04411e77ebf11ae67e690c0e
ashish-panicker <ashish.s.panicker@gmail.com>
2024-04-01 11:49:19 +0530

    Application readme updated for product-service

---
+++ order-service/src/test/java/com/easyskillup/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easyskillup.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
